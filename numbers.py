2362e6c62e1b428823e0c4925ad52663eeea04c8
pausobat <sakesemi@gmail.com>
2017-04-09 18:51:31 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,20 @@
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print("Suma", result)
+
+result = number_one - number_two
+print("Resta", result)
+
+result = number_one * number_two
+print("Multiplicacion", result)
+
+result = number_one / number_two
+print("Division", result)
+
+result = number_one // number_two
+print("Numero entero", result)
+
+result = number_one ** number_two
+print("Exponencial", result)
---
+++ variables.py
@@ -0,0 +1,10 @@
+curso = "Este es el curso de Python3"
+print(curso)
+curso = 8
+print(curso)
+curso = 8.5
+print(curso)
+curso = True
+print(curso)
+curso = "por fin"
+print(curso)
